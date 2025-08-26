226fdf35e2e171806b8bb6ab6f889f97b3a46c02
LordNtlaloe <ntlal0e182@gmail.com>
2025-08-26 12:01:02 +0200

    Candidates Table Added

---
+++ app/(protected)/(admin)/recruitment/candidates/page.tsx
@@ -0,0 +1,10 @@
+import CandidatesTable from '@/components/candidates/candidates-table'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <CandidatesTable />
+        </div>
+    )
+}
